f1dcdc39859da554de6b540afc266fe8e918d68f
medinadiegoeverardo <diegoedina.a@gmail.com>
2019-11-08 13:31:55 -0700

    update

--- Sprint/other.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('northwind_small.sqlite3')
-lite_cursor = conn.cursor()
-
-lite_cursor.execute('SELECT productname, MAX(unitprice) FROM product;').fetchall()
